2d3a032aa990bb09a2ba5f909e6ad975acc1d7f2
Carlos Ziegler <carlos.ziegler@lofino.de>
2021-03-10 15:39:02 +0100

    add some example data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import axios from 'axios'
+import useSWR from 'swr'
+
+const fetcher =( url:string )=> axios.get(url)
 
 
 export default function Home() {
+    const { data, error } = useSWR(`https://swapi.dev/api/people/1/`, fetcher);
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+    const {name,birth_year, gender }  = data.data
+   
     return (
         <div>
-            Home
+            <p id="name">Name: {name}</p>
+            <p id="birth">Birth: {birth_year}</p>
+            <p id="gender">Gender: {gender}</p>
         </div>
     )
 }
